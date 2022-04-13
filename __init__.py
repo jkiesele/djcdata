b4456556c3e67baed75cf6137e6774fed96d2abd
Jan Kieseler <jkiesele@cern.ch>
2022-04-13 11:02:15 +0200

    build structure

--- __init__.py
+++ __init__.py
@@ -1,15 +1,6 @@
 
-
-        
-import sys
-import tensorflow as tf
-sys.modules["keras"] = tf.keras
-
-__version__ = '3.2'
-
 #shortcuts 
 from .TrainData import TrainData
 from .SimpleArray import SimpleArray
 from .DataCollection import DataCollection
-from .Weighter import Weighter
 
--- docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-docker build -t cernml4reco/deepjetcore3:latest . --build-arg BUILD_DATE="$(date)"
