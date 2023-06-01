ca5adfb95e9c2ef39a19a29a798d4790e6b1c562
Jan Kieseler <jkiesele@cern.ch>
2023-06-01 15:56:27 +0200

    added more strict cmake requirement

--- setup.py
+++ setup.py
@@ -25,6 +25,9 @@ class CMakeBuild(build_ext):
                 "CMake must be installed to build the following extensions: " +
                 ", ".join(e.name for e in self.extensions))
 
+        if cmake_version < '3.0.0':
+                raise RuntimeError("CMake >= 3.0.0 is required")
+
         if platform.system() == "Windows":
             cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
                                          out.decode()).group(1))
